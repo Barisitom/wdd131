0a76ac722cb8b18a30fa08d8743b3de9e7174067
Barisitom <53186094+Barisitom@users.noreply.github.com>
2024-05-01 04:27:32 +0100

    Add files via upload

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year and populate footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Populate last modified date
+document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
